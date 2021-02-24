cb5d50fdb7357cd5d5028424e16d323ef113cf4c
dgabruu <68552249+dgabruu@users.noreply.github.com>
2021-02-24 16:06:52 +0530

    Add files via upload

---
+++ precache-manifest.5d8dfe8f7d129a6bfc525369a2d1f3cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c438000c452c06f9a5a148623b3abd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ac0bb73230dc1a37e20",
+    "url": "/static/css/main.76cb3965.chunk.css"
+  },
+  {
+    "revision": "695fe8753ed73bbcef09",
+    "url": "/static/js/2.9e913a95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9e913a95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ac0bb73230dc1a37e20",
+    "url": "/static/js/main.6ec7ac21.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "076553b707db6f62a3ac6b327fc99eaa",
+    "url": "/static/media/ravi.076553b7.jpg"
+  }
+]);
